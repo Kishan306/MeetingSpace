2512bb4bb39ff2b706866e0c3d9ec842c11250ec
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2024-08-22 11:19:17 +0530

    Add new font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

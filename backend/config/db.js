1d93caaaa5ce1d29825bf0559bb57ec594753642
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2025-02-24 10:28:44 +0530

    fix docker issue and containerize app

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,20 @@ const mysql = require('mysql2');
 require("dotenv").config();
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: 'root',
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: 3306
+});
+
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the database!');
+     // Don't forget to release the connection if you test manually
+  }
 });
 
 module.exports = connection.promise();
